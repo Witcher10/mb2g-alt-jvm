5b65c1f2e48d37bde609b91129e1bfa38ffc1cf8
Witcher10 <pandeyvikas10@hotmail.com>
2024-03-28 17:55:11 +0530

    Java 21 changes and scala 3 to work with maven.

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,7 +1,12 @@
-class HelloWorld {
+
+object HelloWorld {
   def main(args: Array[String]): Unit = {
     val hi = new JavaHelloWorld
 
     println(hi.getHello)
+    testPrint
   }
+
+  def testPrint:Unit =
+    println("I am following identing style")
 }
